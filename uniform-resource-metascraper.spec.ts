a661a2ac201cf3b1fb71b153e07db564451b809a
Shahid N. Shah <shahid@shah.org>
2020-08-23 20:32:08 -0400

    Moved Metascraper into its own module

--- uniform-resource-metascraper.spec.ts
+++ uniform-resource-metascraper.spec.ts
@@ -1,5 +1,6 @@
 import * as p from "@shah/ts-pipe";
 import * as ur from "@shah/uniform-resource";
+import * as urMs from "./uniform-resource-metascraper";
 import { Expect, Test, TestFixture, Timeout } from "alsatian";
 
 @TestFixture("Uniform Resource Test Suite")
@@ -7,11 +8,11 @@ export class TestSuite {
     @Timeout(45000)
     @Test("Test a single, valid, UniformResourceContent for Metascraper meta data")
     async testMetascraperResults(): Promise<void> {
-        const tr = p.pipe(new ur.FollowRedirectsGranular(), ur.EnrichMetascraperResults.commonRules);
+        const tr = p.pipe(new ur.FollowRedirectsGranular(), urMs.EnrichMetascraperResults.commonRules);
         const resource = await ur.acquireResource({ uri: "https://www.foxnews.com/lifestyle/photo-of-donald-trump-look-alike-in-spain-goes-viral", transformer: tr });
         Expect(resource).toBeDefined();
-        Expect(ur.isMetascraperResultsSupplier(resource)).toBe(true);
-        if (ur.isMetascraperResultsSupplier(resource)) {
+        Expect(urMs.isMetascraperResultsSupplier(resource)).toBe(true);
+        if (urMs.isMetascraperResultsSupplier(resource)) {
             Expect(resource.metascraperResults).toBeDefined();
             //console.dir(resource.metascraperResults);
         }
